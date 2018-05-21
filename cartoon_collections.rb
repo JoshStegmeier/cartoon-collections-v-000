73bdfc30e26472a3e6a773179d46b8279f3fcf40
Josh Stegmeier <josh@steg.me>
2018-05-21 00:05:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(array)
   newArray = array.collect { | planeteer | planeteer.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  newArray = array.any?( | word | word > 4 )
 end
 
 def find_the_cheese# code an argument here
